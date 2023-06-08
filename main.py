9fdc194fb3b2c2bbaf59920d6d620025d7717993
Alan <tyujn24@mail.ru>
2023-06-08 20:10:13 +0300

    add logger

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from model import poly_model
 from setting import DatabaseConnection
 import logging
+import sys
 
 
 def main():
+    log = logging.getLogger()
+    log.setLevel(logging.INFO)
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    log.addHandler(stdout_handler)
+
     y = [27, 6, 72, 4, 7, -86, -10, 24, -14, -92]
     x = [13, 66, -3, -99, 22, 38, 57, -85, -92, 85]
     degree = 3
